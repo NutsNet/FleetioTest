7f09086cb28ad4c6ebc2431f4dc1a2a1f4cae699
Christophe Vichery <vichery.christophe@gmail.com>
2021-07-15 19:36:56 -0700

    Create map label

--- FleetioTest/MapView.swift
+++ FleetioTest/MapView.swift
@@ -10,6 +10,7 @@ import MapKit
 
 class MapView: UIView {
     let mapMkVi = MKMapView()
+    let mapLb = UILabel()
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -37,5 +38,19 @@ class MapView: UIView {
         let vtMapMkViCst = NSLayoutConstraint(item: mapMkVi, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 4)
         let vbMapMkViCst = NSLayoutConstraint(item: mapMkVi, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: -32)
         NSLayoutConstraint.activate([hlMapMkViCst, hrMapMkViCst, vtMapMkViCst, vbMapMkViCst])
+        
+        // Label
+        mapLb.textColor = .black
+        mapLb.text = "fuel entries"
+        mapLb.textAlignment = .center
+        mapLb.font =  UIFont(name: "AlegreyaSans-Bold", size: 32)
+        mapLb.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(mapLb)
+        
+        let hlMapLbCst = NSLayoutConstraint(item: mapLb, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1, constant: 0)
+        let hrMapLbCst = NSLayoutConstraint(item: mapLb, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1, constant: 0)
+        let vtMapLbCst = NSLayoutConstraint(item: mapLb, attribute: .top, relatedBy: .equal, toItem: mapMkVi, attribute: .bottom, multiplier: 1, constant: 0)
+        let vbMapLbCst = NSLayoutConstraint(item: mapLb, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 0)
+        NSLayoutConstraint.activate([hlMapLbCst, hrMapLbCst, vtMapLbCst, vbMapLbCst])
     }
 }
